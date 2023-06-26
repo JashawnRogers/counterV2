62e13fbe2f1fe427bb784486359ff5817b310fc2
Jashawn <rogersjashawn20@gmail.com>
2023-06-25 17:51:05 -0700

    Add user input to table on click event

--- helpers.js
+++ helpers.js
@@ -1,4 +1,5 @@
 const getElem = (id) => document.getElementById(id);
+const createElem = (elem) => document.createElement(elem);
 
 const on = R.curry(function (eventType, element, callback) {
   element.addEventListener(eventType, callback);
@@ -11,16 +12,52 @@ const on = R.curry(function (eventType, element, callback) {
 const plus = (elem) => {
   on("click", elem, () => {
     const total = ++elem.previousElementSibling.value;
-    console.log(total);
   });
 };
 
 const minus = (elem) => {
   on("click", elem, () => {
     const total = --elem.nextElementSibling.value;
-    console.log(total);
   });
 };
+
+const todaysDate = () => {
+  const today = new Date();
+  const date =
+    today.getMonth() +
+    1 +
+    " / " +
+    today.getDate() +
+    " / " +
+    today.getFullYear();
+  return date;
+};
+
+on("click", getElem("saveEntry"), (e) => {
+  e.preventDefault();
+  const foundTotal = getElem("foundTotal").value;
+  const notFoundTotal = getElem("notFoundTotal").value;
+  const archiveTotal = getElem("archiveTotal").value;
+  const total =
+    parseInt(foundTotal) + parseInt(notFoundTotal) + parseInt(archiveTotal);
+
+  const table = getElem("tableBody");
+  const tableRow = table.insertRow(-1);
+
+  let c1 = tableRow.insertCell(0);
+  let c2 = tableRow.insertCell(1);
+  let c3 = tableRow.insertCell(2);
+  let c4 = tableRow.insertCell(3);
+  let c5 = tableRow.insertCell(4);
+
+  c1.innerText = total;
+  c2.innerText = foundTotal;
+  c3.innerText = notFoundTotal;
+  c4.innerText = archiveTotal;
+  c5.innerText = todaysDate();
+
+  table.appendChild(tableRow);
+});
 
 //Event handlers
 //  account registration and sign in
