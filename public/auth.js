32da55c49f3fbcf7b36fcde688f65ca8bc636878
Jashawn <rogersjashawn20@gmail.com>
2023-07-10 19:40:44 -0700

    Added full sign in/sign out and login state change

--- public/auth.js
+++ public/auth.js
@@ -3,11 +3,11 @@ import { getElem, on } from "/app.js";
 const userSignUp = async (e) => {
   e.preventDefault();
   const signUpUserName = `${getElem("signUpUserName").value}@tasktracker.com`;
-  const signUpPassword = getElem("signUpPassword").value;
+  const signUpUserPassword = getElem("signUpUserPassword").value;
 
   firebase
     .auth()
-    .createUserWithEmailAndPassword(signUpUserName, signUpPassword)
+    .createUserWithEmailAndPassword(signUpUserName, signUpUserPassword)
     .then((userCredential) => {
       const user = userCredential.user;
       console.log(user);
@@ -20,4 +20,67 @@ const userSignUp = async (e) => {
     });
 };
 
+const userSignIn = async (e) => {
+  e.preventDefault();
+  const signInUserName = `${getElem("signInUserName").value}@tasktracker.com`;
+  const signInUserPassword = getElem("signInUserPassword").value;
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(signInUserName, signInUserPassword)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+      alert("successfull sign in");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorCode, errorMessage);
+    });
+};
+
+const userSignOut = async () => {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      alert("successfull sign out");
+      console.log("successfull sign out");
+    })
+    .catch((error) => {
+      console.log(error, "error signing out");
+    });
+};
+
+const checkAuthState = async () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // signed in
+      getElem("authSection").classList.add("display-none");
+      getElem("counterSection").classList.replace(
+        "display-none",
+        "display-block"
+      );
+      getElem("headerSignInBtn").classList.add("display-none");
+      getElem("headerSignOutBtn").classList.replace(
+        "display-none",
+        "display-block"
+      );
+      console.log("signed in");
+    } else {
+      // signed out
+      getElem("counterSection").classList.replace(
+        "display-block",
+        "display-none"
+      );
+      getElem("authSection").classList.replace("display-none", "display-block");
+      console.log("signed out");
+    }
+  });
+};
+
+checkAuthState();
+on("click", getElem("headerSignOutBtn"), userSignOut);
 on("click", getElem("signUpSubmitBtn"), userSignUp);
+on("click", getElem("signInSubmitBtn"), userSignIn);
